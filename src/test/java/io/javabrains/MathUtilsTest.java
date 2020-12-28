f1fe33d5179f6003f58295b14081e579a00f5ac3
Bharath Chamala <bharathreddycv26@gmail.com>
2020-12-28 12:24:31 -0700

    NestedTag, TestInfo, TestReporter,assertAll Lambda

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -7,9 +7,12 @@ import org.junit.jupiter.api.condition.OS;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.*;
 
+@DisplayName("When Running MathUtils")
 class MathUtilsTest {
 
     MathUtils mathUtils;
+    TestInfo testInfo;
+    TestReporter testReporter;
 
     @BeforeAll
     static  void beforeAllInit(){
@@ -17,9 +20,13 @@ class MathUtilsTest {
     }
 
     @BeforeEach
-    void init(){
+    void init(TestInfo testInfo, TestReporter testReporter){
+         this.testInfo = testInfo;
+         this.testReporter = testReporter;
          mathUtils = new MathUtils();
          System.out.println("before each");
+        testReporter.publishEntry("Running "+ testInfo.getDisplayName() + " with tags " + testInfo.getTags());
+
     }
 
     @AfterEach
@@ -27,16 +34,30 @@ class MathUtilsTest {
         System.out.println("Cleaning up..");
     }
 
-    @Test
-    @DisplayName("Testing Add Method")
-    void testAdd() {
+    @Nested
+    @DisplayName("Add method")
+    @Tag("Math")
+    class AddTest{
+        @Test
+        @DisplayName("When adding two positive numbers")
+        void testAddPositive() {
+            int expected = 2;
+            int actual = mathUtils.add(1,1);
+            assertEquals(expected, actual, () -> "should return sum"+ expected + "but returned"+ actual);
+        }
 
-        int expected = 2;
-        int actual = mathUtils.add(1,1);
-        assertEquals(expected, actual, "The add method failed");
+        @Test
+        @DisplayName("Testing Add Negative Method")
+        void testAddNegative() {
+
+            assertEquals(-2, mathUtils.add(-1,-1), "The add method failed");
+        }
     }
 
+
+
     @Test
+    @Tag("Math")
     void testDivide(){
         boolean isServerUp = true;
         assumeTrue(isServerUp);
@@ -44,8 +65,22 @@ class MathUtilsTest {
     }
 
     @Test
-    @EnabledOnOs(OS.LINUX)
-    void testComputerCircleRadius(){
+    @DisplayName("multiply method")
+    @Tag("Math")
+    void testMultiply(){
+        testReporter.publishEntry("Running "+ testInfo.getDisplayName() + " with tags " + testInfo.getTags());
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2,2)),
+                () -> assertEquals(6, mathUtils.multiply(2,3))
+
+        );
+    }
+
+    @RepeatedTest(3)
+    @EnabledOnOs(OS.WINDOWS)
+    @Tag("Circle")
+    void testComputerCircleRadius(RepetitionInfo repetitionInfo){
+
         assertEquals(314.1592653589793,mathUtils.computeCircleArea(100), "should return right circle area");
     }
 
